ce38e80c972fcecfc8f65f95a7c85c81c2cc7227
Abdallah <abdallahchaar90@gmail.comh>
2021-01-15 12:33:12 +0100

    ett exampel om hur vi gör en number som string

---
+++ main.js
@@ -0,0 +1,41 @@
+/* gobal alert, console, prompt*/
+
+
+/*
+var myString = "I Love Programming Languages";
+
+console.log(myString);
+*/
+
+/*
+var myString = 120,
+
+    myNewString = (myString);
+
+console.log(myString);
+
+console.log(myNewString);
+*/
+
+/*
+var myString = 120,
+
+    myNewString = myString.toString;
+
+console.log(typeof (myString));
+
+console.log(typeof (myNewString));
+*/
+
+var myString = 120,
+
+    myNewString = String(myString);
+
+console.log(typeof (myString));
+
+console.log(typeof (myNewString));
+
+
+
+
+
